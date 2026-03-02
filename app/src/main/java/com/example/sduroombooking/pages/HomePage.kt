a82eee4c603d293bce23549c165e4531781bfcc3
sofie <sofim23@student.sdu.dk>
2026-03-02 22:08:21 +0100

    home page finished (look wise)

--- app/src/main/java/com/example/sduroombooking/pages/HomePage.kt
+++ app/src/main/java/com/example/sduroombooking/pages/HomePage.kt
@@ -1,28 +1,63 @@
 package com.example.sduroombooking.pages
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.sduroombooking.bars.NavigationBar
+import com.example.sduroombooking.cards.BookedRoomCard
 import com.example.sduroombooking.navigation.Destination
+import com.example.sduroombooking.viewmodel.UserViewModel
 
 @Composable
 fun HomePage(navController: NavHostController) {
+
+    //midlertidig liste mængde
+    val myBookings = List(5) { it }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        Text("This is Homepage Screen", style = MaterialTheme.typography.headlineMedium)
+        Text(
+            text = "Your bookings",
+            fontSize = 25.sp,
+            style = MaterialTheme.typography.headlineSmall,
+            fontWeight = FontWeight.Bold
+        )
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(onClick = { navController.navigate(Destination.LOGIN.route) }) {
-            Text("Go back to login")
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+            contentPadding = PaddingValues(bottom = 5.dp)
+        )
+        {
+            items(myBookings) {
+                booking ->
+                BookedRoomCard()
+            }
         }
-        Button(onClick = { navController.navigate(Destination.PROFILE.route) }) {
-            Text("Go to Settings")
-        }
+
     }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun HomePagePreview() {
+    val navController = rememberNavController()
+    HomePage(
+        navController = navController
+    )
 }
